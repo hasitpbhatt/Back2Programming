9bdc02715f45ff4456b2ee9c5ad6136613cbfb50
Hasit Bhatt <hasit.p.bhatt@gmail.com>
2015-01-18 01:41:50 +0530

    100 The 3n + 1 problem

---
+++ UVaOnlineJudge/100.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+unsigned int fun(unsigned int n){
+	if(n == 1)
+		return 1;
+	else if(n&1)
+		return 1 + fun(3*n+1);
+	else
+		return 1 + fun(n/2);
+}
+
+int main()
+{
+	unsigned int a,b,mx;
+	while(cin >> a >> b){
+		mx = 0;
+		cout << a << " " << b << " ";
+		if(a>b){
+			unsigned int c = a;
+			a = b;
+			b = c;
+		}
+		for(unsigned int i=a;i<=b;i++){
+			unsigned int n = fun(i);
+			if(n > mx)
+				mx = n;
+		}
+		cout << mx << endl;
+	}
+}
