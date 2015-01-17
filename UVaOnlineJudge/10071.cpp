6e1375384d41b0cab06260506d3cb85255a67b72
Hasit Bhatt <hasit.p.bhatt@gmail.com>
2015-01-18 02:15:20 +0530

    10071 - Back to High School Physics

---
+++ UVaOnlineJudge/10071.cpp
@@ -0,0 +1,12 @@
+// 10071 - Back to High School Physics
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b;
+	while(cin >> a >> b){
+		cout << 2*a*b << endl;
+	}
+	return 0;
+}
